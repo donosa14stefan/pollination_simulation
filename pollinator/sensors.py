e380492f38ff169421ebde006104a28f9d3f921d
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-21 21:33:29 +0300

    Update sensors.py

--- pollinator/sensors.py
+++ pollinator/sensors.py
@@ -1,20 +1,14 @@
-import random
+import numpy as np
 
 class SimulatedSensors:
     def __init__(self):
-        self.gps_error = 2.0  # metri
-        self.altitude_error = 0.5  # metri
+        pass
 
-    def get_gps(self, true_position):
-        x, y, z = true_position
-        return (
-            x + random.uniform(-self.gps_error, self.gps_error),
-            y + random.uniform(-self.gps_error, self.gps_error),
-            z + random.uniform(-self.altitude_error, self.altitude_error)
-        )
+    def get_gps(self, position):
+        return position
 
     def get_battery_level(self):
-        return random.uniform(0, 100)
+        return np.random.randint(20, 100)
 
     def get_velocity(self):
-        return random.uniform(0, 10)  # m/s
+        return np.random.randint(1, 10)
