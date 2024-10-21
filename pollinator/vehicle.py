2d6b1f47566ba51a1f9f1b11e33ef3430e23f6f6
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-21 18:25:25 +0300

    Update vehicle.py

--- pollinator/vehicle.py
+++ pollinator/vehicle.py
@@ -1,180 +1,35 @@
-from dronekit import Vehicle, LocationGlobalRelative
-#from pymavlink import mavutil, mavlink
-import time, sys
-
-WAIT_TIMEOUT = 30
-
-LOG_PREVIOUSLINE = '\033[F'
-_summarize_out_msgs = []
-_summarize_out_time_between = 0.1
+import time
+from dronekit import VehicleMode, LocationGlobalRelative
 
 class VehicleWrapper:
-
-    def __init__(self, vehicle: Vehicle):
+    def __init__(self, vehicle):
         self.vehicle = vehicle
-        self.timeout = WAIT_TIMEOUT
+        self.position = [0, 0, 0]  # x, y, altitude
 
-    def close(self) -> None:
-        pass
+    def move_to(self, x, y, altitude):
+        self.position = [x, y, altitude]
+        # Simulează mișcarea vehiculului
+        time.sleep(2)  # Simulează timpul necesar pentru mișcare
+        print(f"Moved to position: {self.position}")
 
-    '''
-    Summarize the data of interest to output.
-    '''
-    def flash_info(self) -> None:
-        global _summarize_out_msgs
-        for i in range(len(_summarize_out_msgs)):
-            sys.stdout.write(LOG_PREVIOUSLINE)
-
-        _summarize_out_msgs = []
-
-        def msg(msg):
-            _summarize_out_msgs.append(msg)
-        
-        vehicle = self.vehicle
-        # Get some vehicle attributes (state)
-        msg("VEHICLE:")
-        msg(" GPS: {vehicle.gps_0}")
-        msg(" Battery: {vehicle.battery}")
-        msg(" Last Heartbeat: {vehicle.last_heartbeat}")
-        msg(" Is Armable?: {vehicle.is_armable}")
-        msg(" System status: {vehicle.system_status.state}")
-        msg(" Mode: {vehicle.mode.name}")  # settable
-        msg(f" Position: {str(vehicle.location.global_frame)}")  # lat lon alt
-
-        for m in _summarize_out_msgs:
-            print(str(m))
-        time.sleep(_summarize_out_time_between)
-    
-
-    def guided(self):
-        print("Setting guided mode...")
-        self.vehicle.wait_for_mode("GUIDED", timeout=self.timeout)
-        print("Guided mode set.")
-    
-    def is_landed(self):
-        return (not self.vehicle.armed) and (self.vehicle.location.global_relative_frame.alt <= 1)
-    
-    def is_in_flight(self):
-        return self.vehicle.armed and (self.vehicle.location.global_relative_frame.alt > 1)
-
-    def land(self):
-        if not self.vehicle.armed:
-            print("Can't land when vehicle is not armed!")
-            return
-        
-        # get location
-        curr = self.vehicle.location.global_relative_frame
-        print(curr)
-
-        # create destination to altitude=0
-        new = LocationGlobalRelative(curr.lat, curr.lon, 0)
-
-        # set the destination
-        self.vehicle.simple_goto(new)
-
-        # wait
-        print("Waiting for landing...")
-        def landed():
-            return self.vehicle.location.global_relative_frame.alt < 1
-        self.vehicle.wait_for(landed, timeout=self.timeout)
-    
-    def takeoff(self, targetAlt = 10):
-        if not self.vehicle.armed:
-            print("Can't takeoff when vehicle is not armed!")
-        self.vehicle.simple_takeoff(targetAlt)
-    
-    def wait_altitude(self, targetAlt):
+    def takeoff(self, target_altitude):
+        print(f"Taking off to altitude: {target_altitude}")
+        self.vehicle.mode = VehicleMode("GUIDED")
+        self.vehicle.armed = True
+        self.vehicle.simple_takeoff(target_altitude)
         while True:
-            print(" Altitude: ", self.vehicle.location.global_relative_frame.alt, end='\r')
-            print()
-            #Break and return from function just below target altitude.
-            if self.vehicle.location.global_relative_frame.alt >= targetAlt*0.95:
+            print(f"Altitude: {self.vehicle.location.global_relative_frame.alt}")
+            if self.vehicle.location.global_relative_frame.alt >= target_altitude * 0.95:
                 print("Reached target altitude")
                 break
             time.sleep(1)
-    
-    def wait_arm(self):
-        print("Waiting for ready to arm...")
-        self.vehicle.wait_for_armable(self.timeout)
-        print("Armable!")
 
-    def arm(self):
-        if self.vehicle.armed:
-            print("Can't arm a vehicle that is already armed already!")
-            return
-        self.vehicle.arm(self.timeout)
-    
-    def disarm(self):
-        if not self.vehicle.armed:
-            print("Can't disarm when already disarmed!")
-            return
-        print("Disarming")
-        self.vehicle.armed = False
-        def whilearmed():
-            return self.vehicle.armed
-        self.vehicle.wait_for(whilearmed, timeout=self.timeout)
-        print("Disarmed")
+    def land(self):
+        print("Landing...")
+        self.vehicle.mode = VehicleMode("LAND")
+        while self.vehicle.armed:
+            print(f"Altitude: {self.vehicle.location.global_relative_frame.alt}")
+            time.sleep(1)
+        print("Landed")
 
-    def force_disarm(self):
-        if not self.vehicle.armed:
-            print("Can't force disarm when already disarmed!")
-            return
-        self.vehicle.message_factory
-        self.vehicle.send_mavlink()
-    
-    '''
-    Routine: Resets the drone to the ground if its armed/flying.
-    '''
-    def routine_reset(self):
-        if not self.vehicle.armed:
-            print("No need to safe reset, not armed!")
-            return
-        
-        self.guided()
-        self.land()
-        self.disarm()
-        
-        # TODO: Force Disarm - Drone does not disarm?
-        '''print("Sending force disarm")
-        self.vehicle._master.mav.command_long_send(
-                0,  # target_system
-                0,
-                mavlink.MAV_CMD_COMPONENT_ARM_DISARM, # command
-                0, # confirmation
-                0, # param1 (0 to indicate disarm)
-                21196, # param2 (21196=force) (all other params meaningless)
-                0, # param3
-                0, # param4
-                0, # param5
-                0, # param6
-                0) # param7
-        print("Sent force disarm")'''
-
-    def routine_preflight(self):
-        if self.is_in_flight():
-            print("Resetting drone...")
-            self.routine_reset()
-            print("Reset!")
-        self.disarm()
-    
-
-    '''
-    Routine: Land
-    '''
-    def routine_takeoff(self, targetAlt = 10):
-        if self.is_in_flight():
-            print("Can't takeoff, already armed and in-flight!")
-            return
-        
-        print("Takeoff!")
-        print("Setting guided mode...")
-        self.guided()
-        print("Waiting for arm...")
-        self.wait_arm()
-        self.arm()
-        print("Armed. Taking off")
-
-        self.takeoff(targetAlt=targetAlt)
-        print(f"Take off. Waiting to altitude: {targetAlt}")
-        self.wait_altitude(targetAlt=targetAlt)
-        print(f"Reached altitude: {targetAlt}")
+    # Adaugă alte metode necesare pentru controlul vehiculului
